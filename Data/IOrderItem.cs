04e4657765d97872b5ad58c98711060c8ba1d999
Andrew Dulac <dulacand@ksu.edu>
2020-02-26 14:48:55 -0600

    Order Tests

---
+++ Data/IOrderItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// An interface representing a single item in an order
+    /// </summary>
+    public interface IOrderItem
+    {
+        /// <summary>
+        /// The Price of the item.
+        /// </summary>
+        double Price { get; }
+        /// <summary>
+        /// The Special Instructions for the item.
+        /// </summary>
+        List<string> SpecialInstructions { get; }
+
+    }
+}
---
+++ Data/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// An order class which holds data summarizing specific details of an Order.
+    /// </summary>
+    public class Order
+    {
+        /// <summary>
+        /// A group of Order Items that this Order consists of.
+        /// </summary>
+        public IEnumerable<IOrderItem> Items => throw new NotImplementedException();
+
+        public double Subtotal => 0;
+
+        public void Add(IOrderItem OrderItem)
+        {
+
+        }
+
+        public void Remove(IOrderItem OrderItem)
+        {
+
+        }
+
+
+    }
+}
